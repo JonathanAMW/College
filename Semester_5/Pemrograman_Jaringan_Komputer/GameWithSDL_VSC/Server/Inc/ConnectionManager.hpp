8a0477971fb0fe966ca0390464f40c5c4e519289
PJacketG <anantamiyoruwijaya2@gmail.com>
2023-12-07 19:10:12 +0700

    Update Latency Handling

--- Semester_5/Pemrograman_Jaringan_Komputer/GameWithSDL_VSC/Server/Inc/ConnectionManager.hpp
+++ Semester_5/Pemrograman_Jaringan_Komputer/GameWithSDL_VSC/Server/Inc/ConnectionManager.hpp
@@ -20,10 +20,13 @@ public:
     int Init(std::string connectionType);
     int RunAsServer();
     int RunAsClient();
-    // int SendData(const GameState& gameState, ClientMode clientMode);
-    // int ReceiveData(GameState& gameState, ClientMode clientMode);
     bool SendData(const GameState& gameState, ClientMode clientMode);
     bool ReceiveData(GameState& gameState, ClientMode clientMode);
+
+    std::chrono::milliseconds GetLatency() const {
+        return std::chrono::duration_cast<std::chrono::milliseconds>(lastReceivedTime - lastSentTime);
+    }
+
 
 private:
     TCPsocket serverSocket;
@@ -32,7 +35,9 @@ private:
 
     std::chrono::steady_clock::time_point lastSentTime;
     std::chrono::steady_clock::time_point lastReceivedTime;
-    std::queue<std::pair<std::chrono::steady_clock::time_point, GameState>> receivedDataQueue;
+    // std::queue<std::pair<std::chrono::steady_clock::time_point, GameState>> receivedDataQueue;
+
+
 };
 
 #endif // CONNECTION_MANAGER_HPP
--- Semester_5/Pemrograman_Jaringan_Komputer/GameWithSDL_VSC/Server/src/ConnectionManager.cpp
+++ Semester_5/Pemrograman_Jaringan_Komputer/GameWithSDL_VSC/Server/src/ConnectionManager.cpp
@@ -1,5 +1,8 @@
 #include "ConnectionManager.hpp"
 
+#include <memory>
+
+
 const char* SERVER_IP = "127.0.0.1"; // Change this to the server's IP
 
 ConnectionManager::ConnectionManager() : serverSocket(nullptr), clientSocket(nullptr) {
@@ -21,6 +24,7 @@ ConnectionManager::~ConnectionManager() {
     SDLNet_Quit();
 }
 
+#pragma region Socket implementation
 int ConnectionManager::Init(std::string connectionType) {
     if (connectionType == "Server") {
         return RunAsServer();
@@ -30,6 +34,8 @@ int ConnectionManager::Init(std::string connectionType) {
         return -1;
     }
 }
+#pragma endregion
+
 
 int ConnectionManager::RunAsServer() {
     IPaddress serverIP;
@@ -83,6 +89,64 @@ int ConnectionManager::RunAsClient() {
 }
 
 
+bool ConnectionManager::SendData(const GameState& gameState, ClientMode connectionType) {
+    char buffer[sizeof(GameState) + sizeof(std::chrono::steady_clock::time_point)]; // Extra space for time
+
+    // Serialize game state
+    gameState.Serialize(buffer, sizeof(GameState));
+
+    // Serialize current time
+    std::chrono::steady_clock::time_point currentTime = std::chrono::steady_clock::now();
+    memcpy(buffer + sizeof(GameState), &currentTime, sizeof(std::chrono::steady_clock::time_point));
+
+    // Send data
+    if ((connectionType == ClientMode::SERVER && serverSocket) ||
+        (connectionType == ClientMode::CLIENT && clientSocket)) {
+        int sentBytes = SDLNet_TCP_Send(clientSocket, buffer, sizeof(buffer));
+        if (sentBytes <= 0) {
+            std::cerr << "Failed to send data: " << SDLNet_GetError() << std::endl;
+            return false; // Data not sent
+        }
+        lastSentTime = currentTime; // Update last sent time
+        return true; // Data sent successfully
+    } else {
+        std::cerr << "Unknown connection type or no socket available in Send" << std::endl;
+        return false; // Unknown connection type or no socket available
+    }
+}
+
+bool ConnectionManager::ReceiveData(GameState& gameState, ClientMode connectionType) {
+    char buffer[sizeof(GameState) + sizeof(std::chrono::steady_clock::time_point)]; // Extra space for time
+
+    // Receive data
+    if ((connectionType == ClientMode::SERVER || connectionType == ClientMode::CLIENT) && clientSocket) {
+        int receivedBytes = SDLNet_TCP_Recv(clientSocket, buffer, sizeof(buffer));
+
+        if (receivedBytes > 0) {
+            // Deserialize game state
+            gameState.Deserialize(buffer, sizeof(GameState));
+
+            // Deserialize received time
+            std::chrono::steady_clock::time_point receivedTime;
+            memcpy(&receivedTime, buffer + sizeof(GameState), sizeof(std::chrono::steady_clock::time_point));
+
+            lastReceivedTime = receivedTime; // Update last received time
+
+            // Calculate and print the Round-Trip Time (RTT)
+            std::chrono::milliseconds rtt = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - receivedTime);
+            std::cout << "Round-Trip Time (RTT): " << rtt.count() << " ms" << std::endl;
+
+            return true; // Data received successfully
+        }
+    }
+
+    return false; // No data received or unknown connection type
+}
+
+
+
+
+/*
 bool ConnectionManager::SendData(const GameState& gameState, ClientMode connectionType) 
 {
     char buffer[sizeof(GameState)];
@@ -125,16 +189,20 @@ bool ConnectionManager::ReceiveData(GameState& gameState, ClientMode connectionT
             gameState.Deserialize(buffer, sizeof(buffer));
 
             auto currentTime = std::chrono::steady_clock::now();
-            receivedDataQueue.push(std::make_pair(currentTime, gameState));
+            // receivedDataQueue.push(std::make_pair(currentTime, gameState));
 
             lastReceivedTime = currentTime;
+            
+            // Calculate and print the Round-Trip Time (RTT)
+            std::chrono::milliseconds rtt = GetLatency();
+            std::cout << "Round-Trip Time (RTT): " << rtt.count() << " m/s" << std::endl;
             return true; // Data received successfully
         }
     }
 
     return false; // No data received or unknown connection type
 }
-
+*/
 
 /*
 bool ConnectionManager::SendData(const GameState& gameState, ClientMode connectionType) {
